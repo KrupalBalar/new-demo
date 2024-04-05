a799f20aac316169ab4685b8ff83ead1b4d56294
KrupalBalar <107741087+KrupalBalar@users.noreply.github.com>
2024-04-05 06:38:46 +0000

    array and object

--- 01_basic/object.js
+++ 01_basic/object.js
@@ -27,4 +27,17 @@ var user2 = {
     six: 6
 }
 var user3 = Object.assign(user1, user2);
-console.log("object data:::", user3);
+// var user3 = {...user1, ...user2}
+// console.log("object data:::", user3);
+// console.log(Object.keys(user3));
+// console.log(Object.values(user3));
+// console.log(Object.entries(user3));
+
+  
+var user = {
+    name: 'krupal',
+    age: 22,
+    state: 'Gujrat',
+    email: 'krupal@google.com',
+    isLoggedIn: false
+}
